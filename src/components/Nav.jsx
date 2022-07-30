6dd9f35ac7449f47da01a048d1096336b7e7eb44
Gustavo <garancibiacl@gmail.com>
2022-07-30 19:51:03 -0400

    Se crea efecto icono nav y menu lateral

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,51 +1,55 @@
 import React from "react";
 import logo from "../img/logo.svg";
-import Hamburger from 'hamburger-react'
+import Hamburger from "hamburger-react";
 // import { useState } from 'react';
 // import { SlButton, SlDrawer } from '@shoelace-style/shoelace/dist/react';
 
 // import { Drawer, ANCHOR } from "baseui/drawer";
+import ReactDOM from "react-dom";
 
 function Nav() {
-
-  // const [isOpen, setIsOpen] = React.useState(false);
-
   return (
-
-      <div className="nav ">
-        
-
-      <a class="nav__link" href="#"  >
-      <Hamburger size={30}    />
+    <div className="nav ">
+      <div class="hamburger-menu">
+        <input id="menu__toggle" type="checkbox" />
+        <label class="menu__btn" for="menu__toggle">
+          <span></span>
+        </label>
+        <ul class="menu__box">
+          <li>
+            <a class="menu__item" href="#">
+              Home
+            </a>
+          </li>
+          <li>
+            <a class="menu__item" href="#">
+              About
+            </a>
+          </li>
+          <li>
+            <a class="menu__item" href="#">
+              Team
+            </a>
+          </li>
+          <li>
+            <a class="menu__item" href="#">
+              Contact
+            </a>
+          </li>
+          <li>
+            <a class="menu__item" href="#">
+              Instagram
+            </a>
+          </li>
+        </ul>
+      </div>
+      <a class="nav__logo__container" href="#">
+        <img class="nav__logo" src={logo} width="230px" alt="" />
       </a>
-
-   
       <a class="nav__link" href="#">
-        <img class="nav__logo" src={logo} width="230px" alt='' />
+        <box-icon name="search-alt" color="#414141" size={30}></box-icon>
       </a>
-  
-
-
-      <a class="nav__link" href="#">
-        <box-icon name="search-alt" color="#414141" size={30} ></box-icon>
-      </a>
-
-
-
-  
-
-    {/* <>
-    <Drawer
-      isOpen={isOpen}
-      autoFocus
-      onClose={() => setIsOpen(false)}
-      anchor={ANCHOR.left}
-    >
-      <div>drawer content</div>
-    </Drawer>
-    </> */}
-   </div>
-
+    </div>
   );
 }
 
